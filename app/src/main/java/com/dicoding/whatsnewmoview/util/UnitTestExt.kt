d80dc21de0810c4d61a1e7914c147681e052b046
W Rangga <willybrodus.khaisar@iconpln.co.id>
2021-06-14 20:31:18 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/whatsnewmoview/util/UnitTestExt.kt
@@ -0,0 +1,27 @@
+package com.dicoding.whatsnewmoview.util
+
+import androidx.paging.*
+
+suspend fun <T : Any> PagingData<T>.collectData(): List<T> {
+    val dcb = object : DifferCallback {
+        override fun onChanged(position: Int, count: Int) {}
+        override fun onInserted(position: Int, count: Int) {}
+        override fun onRemoved(position: Int, count: Int) {}
+    }
+    val items = mutableListOf<T>()
+    val dif = object : PagingDataDiffer<T>(dcb) {
+        override suspend fun presentNewList(
+            previousList: NullPaddedList<T>,
+            newList: NullPaddedList<T>,
+            newCombinedLoadStates: CombinedLoadStates,
+            lastAccessedIndex: Int,
+            onListPresentable: () -> Unit
+        ): Int? {
+            for (idx in 0 until newList.size)
+                items.add(newList.getFromStorage(idx))
+            return null
+        }
+    }
+    dif.collectFrom(this)
+    return items
+}
