e2ee0dcf7014de347bc9660e5854737a1dd4e203
willy.rangga <willybrodus.khaisar@iconpln.co.id>
2023-11-18 17:58:10 +0700

    Example nine

--- app/src/main/java/com/dicoding/whatsnewmoview/widget/ImageRoundCorners.java
+++ app/src/main/java/com/dicoding/whatsnewmoview/widget/ImageRoundCorners.java
@@ -97,7 +97,7 @@ public class ImageRoundCorners extends androidx.appcompat.widget.AppCompatImageV
 
     /*
     * Testing for this yeah. So it's a good think
-    * Add the commit for third
+    * Add the commit for fourth
     * */
     private boolean isRounded(int corner) {
         return (roundedCorners & corner) == corner;
